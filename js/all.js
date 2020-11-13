faf84ac54ee5f2d3dc173034fd7aa91215c9e566
Pickachu Chen <pickachu1006@gmail.com>
2020-11-13 14:55:54 +0800

    20201113_pop up update & delete line btn

--- js/all.js
+++ js/all.js
@@ -20,16 +20,16 @@ $(document).ready(function () {
             $("#fb-root").attr({
                 style: "display: none;"
             });
-            $("#wh-widget-send-button").attr({
-                style: "display: block;width: 261px;height: 94px;"
-            });
-            $(".line_alert").attr({
-                style: "display: block"
-            });
+            // $("#wh-widget-send-button").attr({
+            //     style: "display: block;width: 261px;height: 94px;"
+            // });
+            // $(".line_alert").attr({
+            //     style: "display: block"
+            // });
         } else if ((formTop <= (scrollPos + windowHeight / 2)) && (width > 768)) {
-            $("#wh-widget-send-button").attr({
-                style: "display: none ;width: 0px;height: 0px;"
-            });
+            // $("#wh-widget-send-button").attr({
+            //     style: "display: none ;width: 0px;height: 0px;"
+            // });
             $("#fb-root").attr({
                 style: "display: block;"
             });
@@ -37,16 +37,10 @@ $(document).ready(function () {
     });
 
     //偵測sns line btn 手機版還是電腦版，連結不同
-    var vw=$(window).width();
-    if (vw <= 768) {
-        $(".line_link").attr("href", "https://line.me/R/oaMessage/@fhl1857j/?%E6%9F%A5%E8%A9%A2%E8%80%81%E9%A1%A7%E5%AE%A2%E7%8D%A8%E4%BA%AB%E5%84%AA%E6%83%A0");
-    } else {
-        $(".line_link").attr("href", "https://line.me/R/ti/p/%40fhl1857j");
-    }
+    // var vw=$(window).width();
+    // if (vw <= 768) {
+    //     $(".line_link").attr("href", "https://line.me/R/oaMessage/@fhl1857j/?%E6%9F%A5%E8%A9%A2%E8%80%81%E9%A1%A7%E5%AE%A2%E7%8D%A8%E4%BA%AB%E5%84%AA%E6%83%A0");
+    // } else {
+    //     $(".line_link").attr("href", "https://line.me/R/ti/p/%40fhl1857j");
+    // }
 });
-
-// 查詢老顧客獨享優惠
-// https://line.me/R/oaMessage/@fhl1857j/?%E6%9F%A5%E8%A9%A2%E8%80%81%E9%A1%A7%E5%AE%A2%E7%8D%A8%E4%BA%AB%E5%84%AA%E6%83%A0
-
-// 索取最新優惠
-// https://line.me/R/oaMessage/@fhl1857j/?%E7%B4%A2%E5%8F%96%E6%9C%80%E6%96%B0%E5%84%AA%E6%83%A0
